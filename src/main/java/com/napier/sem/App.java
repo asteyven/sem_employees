153910e459ef8959d2cfca4d93928beab99cbf3d
kevinsim <k.sim@napier.ac.uk>
2023-07-03 10:57:29 +0100

    delay

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -39,12 +39,12 @@ public class App
             System.exit(-1);
         }
 
-        int retries = 10;
+        int retries = 50;
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(10);
+                Thread.sleep(10000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
